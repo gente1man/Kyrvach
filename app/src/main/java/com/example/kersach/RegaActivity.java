ae061e72b81fe32faf0f9474a8b14008b2c71650
Ponomarev <ponomarevdanila688@gmail.com>
2024-05-25 00:12:02 +0300

    Kyrsach

---
+++ app/src/main/java/com/example/kersach/RegaActivity.java
@@ -0,0 +1,14 @@
+package com.example.kersach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rega);
+    }
+}
